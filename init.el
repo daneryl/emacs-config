6e1645532bb686b32672192b164771f68b982fb4
daneryl <daneryl@gmail.com>
2017-05-15 17:41:33 +0200

    first commit

---
+++ init.el
@@ -0,0 +1,143 @@
+(setq inhibit-startup-screen t)
+(load-theme 'wombat)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(electric-pair-mode 1)
+(show-paren-mode 1)
+(global-linum-mode 1)
+(set-cursor-color "#ffffff") 
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :init
+  (progn
+    (require 'helm-config)
+    (setq helm-candidate-number-limit 100)
+    ;; From https://gist.github.com/antifuchs/9238468
+    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
+	  helm-input-idle-delay 0.01  ; this actually updates things
+					; reeeelatively quickly.
+	  helm-yas-display-key-on-candidate t
+	  helm-quick-update t
+	  helm-M-x-requires-pattern nil
+	  helm-ff-skip-boring-files t)
+    (helm-mode))
+  :bind (("C-c h" . helm-mini)
+	 ("C-h a" . helm-apropos)
+	 ("C-x C-b" . helm-buffers-list)
+	 ("C-x b" . helm-buffers-list)
+	 ("M-y" . helm-show-kill-ring)
+	 ("M-x" . helm-M-x)
+	 ("C-x c o" . helm-occur)
+	 ("C-x c SPC" . helm-all-mark-rings)))
+(ido-mode -1)
+
+(use-package projectile
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (custom-set-faces
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+   '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+   '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+   '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
+  :init
+  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package evil
+  :ensure t
+  :config
+  (use-package evil-surround
+    :ensure t)
+  (use-package evil-nerd-commenter
+    :ensure t)
+  (use-package evil-leader
+    :ensure t
+    :config
+    (evil-leader/set-leader ",")
+    (evil-leader/set-key
+      "q" 'kill-this-buffer
+      "c SPC" 'evilnc-comment-or-uncomment-lines
+      "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+      "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+      "cc" 'evilnc-copy-and-comment-lines
+      "cp" 'evilnc-comment-or-uncomment-paragraphs))
+  (evil-mode)
+  (global-evil-surround-mode 1)
+  (global-evil-leader-mode))
+
+
+(use-package company
+  :ensure t
+  :init (add-hook 'after-init-hook 'global-company-mode))
+
+(global-set-key (kbd "C-s") (lambda() (interactive) (save-buffer) (evil-force-normal-state)))
+(global-set-key (kbd "C-j") (lambda() (interactive) (next-line 5)))
+(global-set-key (kbd "C-k") (lambda() (interactive) (previous-line 5)))
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+      (progn
+	;; use 120 char wide window for largeish displays
+	;; and smaller 80 column windows for smaller displays
+	;; pick whatever numbers make sense for you
+	(if (> (x-display-pixel-width) 1280)
+	    (add-to-list 'default-frame-alist (cons 'width 203))
+	  (add-to-list 'default-frame-alist (cons 'width 80)))
+	;; for the height, subtract a couple hundred pixels
+	;; from the screen height (for panels, menubars and
+	;; whatnot), then divide by the height of a char to
+	;; get the height we want
+	(add-to-list 'default-frame-alist 
+		     (cons 'height (/ (- (x-display-pixel-height) 200)
+				      (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
